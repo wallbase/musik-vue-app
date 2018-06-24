dc78b539aaa8e4ceab23a1bd9633909940459053
wangkun23 <wang.kun@jinyinwu.com>
2018-06-24 15:34:02 +0800

    icons

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Dashboard from '@/components/Dashboard'
 import Blog from '@/components/Blog'
+import Icons from '@/components/Icons'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path: '/', component: Dashboard},
-    {path: '/blog', component: Blog}
+    {path: '/blog', component: Blog},
+    {path: '/icons', component: Icons}
   ]
 })
