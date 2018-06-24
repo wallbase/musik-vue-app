380ddc24d2fe30e050cacd2a9f459c0bcac4e32e
wangkun23 <wang.kun@jinyinwu.com>
2018-06-24 15:08:03 +0800

    添加uiv组件

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import * as uiv from 'uiv'
+//less
+import '@/less/app.less'
 
+Vue.use(uiv)
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
